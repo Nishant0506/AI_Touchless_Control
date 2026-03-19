7aa570bc2c94d0e499cd63eaeed3aa181f738068
Nishant0506 <2330096@kiit.ac.in>
2026-03-19 20:25:48 +0530

    Prepare for cloud deployment: disable debug mode, add Procfile

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 import threading
+import os
 from gesture_control import start_gesture
 from voice_control import start_voice
 from utils import SystemState
@@ -53,4 +54,4 @@ def get_log():
     return jsonify({'log': state.log[-20:]})  # Last 20 log entries
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
